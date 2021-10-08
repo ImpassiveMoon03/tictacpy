02ca49e5d43ba5f1041d66d72737d986ff878106
ImpassiveMoon03 <offordjr129@gmail.com>
2021-10-07 23:51:56 -0400

    Fixed bad classifier

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 classifiers = [
   'Development Status :: 5 - Production/Stable',
   'Intended Audience :: Developers',
-  'Operating System :: Linux',
   'License :: OSI Approved :: MIT License',
   'Programming Language :: Python :: 3'
 ]
